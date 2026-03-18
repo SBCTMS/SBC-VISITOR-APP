0e3cee4dd600720a540be2a28490521264b2e717
SBCTMS <danielbala26@gmail.com>
2026-03-18 16:05:39 +0530

    Update cache file paths for SBC Visitor App

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'sbc-visitor-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = ['/SBC-VISITOR-APP/', '/SBC-VISITOR-APP/index.html', '/SBC-VISITOR-APP/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
